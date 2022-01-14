a7fa46c8c5d7d1a816fe1b42e71cf537f6a4b279
jhhkim <aekim7@naver.com>
2022-01-14 17:55:47 +0900

    검색결과 보여줌

--- src/main/java/com/project/irang_map/web/IndexController.java
+++ src/main/java/com/project/irang_map/web/IndexController.java
@@ -2,12 +2,15 @@ package com.project.irang_map.web;
 
 import java.util.List;
 
+
 import com.project.irang_map.dto.KidsmapDto;
 import com.project.irang_map.dto.KidsmapListResponseDto;
 import com.project.irang_map.service.KidsmapService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
 
@@ -25,5 +28,7 @@ public class IndexController {
     public List<KidsmapListResponseDto> kidsmapFindAll() {
         return kidsmapService.findAll();
     }
+
+    
 
 }
--- src/main/java/com/project/irang_map/web/SearchController.java
+++ src/main/java/com/project/irang_map/web/SearchController.java
@@ -5,23 +5,35 @@ import com.project.irang_map.dto.KidsmapDto;
 
 import com.project.irang_map.dto.KidsmapListResponseDto;
 import com.project.irang_map.service.KidsmapService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class SearchController {
     private final KidsmapService kidsmapService;
 
     @PostMapping("/search")
+    @ResponseBody//json으로 전달하려고
     public List<KidsmapListResponseDto> serch(@RequestParam(value = "juso") String juso){
         //return "입력한 주소는?" + juso; //입력한 값 확인용
         return kidsmapService.findAllByAddr("%" + juso + "%"); // 이런걸로 리턴해야할것같은데
+    }
+
+    //화면에 보여줌
+    @GetMapping("/search")
+    public String serch(Model model, @RequestParam(value = "juso") String juso){
+        model.addAttribute("kidsmap", kidsmapService.findAllByAddr("%" + juso + "%"));
+        return "kidsmap";
     }
 
 }
